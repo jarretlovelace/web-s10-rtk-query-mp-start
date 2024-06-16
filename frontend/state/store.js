42babaaa24e212529247968d4c20c598ce2fa22a
Jarret Lovelace <jarretlovelace@icloud.com>
2024-06-15 21:51:47 -0600

    solution

--- frontend/state/store.js
+++ frontend/state/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import quotesReducer from './quotesSlice'
+import { quotesApi } from './quotesApi'
 
 export const store = configureStore({
   reducer: {
     quotesState: quotesReducer,
-  }
+    [quotesApi.reducerPath]: quotesApi.reducer,
+  },
 })
